661d5fd66b0940d542e9fb7545849faf8a36b7fc
Andrea Fermo <andreafermo.sd@gmail.com>
2023-05-12 15:53:32 +0200

    creazione database, migrate table e struttura

---
+++ database/migrations/2023_05_12_132446_create_trains_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('trains', function (Blueprint $table) {
+            $table->id();
+            $table->string('azienda', '30');
+            $table->string('stazione_di_partenza', '100');
+            $table->string('stazione_di_arrivo', '100');
+            $table->time('orario_di_partenza');
+            $table->time('orario_di_arrivo');
+            $table->unsignedSmallInteger('codice_treno');
+            $table->unsignedTinyInteger('numero_carrozze');
+            $table->boolean('in_orario');
+            $table->boolean('cancellato');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('trains');
+    }
+};
